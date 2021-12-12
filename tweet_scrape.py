6adc094a066eb47271c6d947b926fa0b4bf2798c
anthonysokolov <anthonysokolov20@gmail.com>
2021-12-11 20:50:45 -0500

    write to csv, keep track of timestamps

--- tweet_scrape.py
+++ tweet_scrape.py
@@ -2,6 +2,7 @@ import tweepy
 import time
 import json
 import sys
+import pandas as pd
 
 from tweepy import Stream
 
@@ -13,19 +14,33 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-filename = "_".join(sys.argv[1].split(" "))
+filename = "_".join(sys.argv[1].split(" ")) + ".csv"
 print("Collecting tweets for:", sys.argv[1])
 print("Writing results to", filename)
 
+try:
+    df = pd.read_csv("./data/{}".format(filename))
+    tweets = df["tweets"]
+    timestamps = df["timestamps"]
+
+except Exception:
+    tweets = pd.Series()
+    timestamps = pd.Series()
+    
 class Listener(Stream):
     def on_data(self,data):
         try:
-            text = json.loads(data.decode('utf8'))['text']
-            with open("./data/{}.txt".format(filename),"a") as f:
-                f.write(text)
+            resp = json.loads(data.decode('utf8'))
+
+            tweets = tweets.append(pd.Series(resp["text"]))
+            timestamps = timestamps.append(pd.Series(resp["created_at"]))
 
             self.count += 1
             print("collected",self.count,"tweets")
+
+            if self.count % 10 == 0:
+                df = pd.DataFrame([tweets, timestamps], columns=["tweets","timestamps"])
+                df.to_csv(filename)
 
         except Exception as e:
             print(e)
